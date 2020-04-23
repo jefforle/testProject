b7bf952c987889e4fa8ab8a2fde3197234f282d3
jefforle <jefforle20@gmail.com>
2020-04-23 15:31:34 +0900

    .gitignore 적용

--- src/main/java/com/springboot/test/testProject/config/JpaConfig.java
+++
@@ -1,6 +0,0 @@
-package com.springboot.test.testProject.config;
-
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
-
-@EnableJpaAuditing //JPA Auditing 활성화
-public class JpaConfig {}
