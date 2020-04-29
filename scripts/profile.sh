684ed818eab80b45a37bc6912fdd4c8ba70ddd6f
jefforle <jefforle20@gmail.com>
2020-04-29 19:08:35 +0900

    무중단 배포 스크립트(.sh) 작성

--- scripts/profile.sh
+++ scripts/profile.sh
@@ -19,16 +19,16 @@ function find_idle_profile() {
       IDLE_PROFILE=real1
     fi
     echo "${IDLE_PROFILE}"
+}
 
-    # 쉬고 있는 profile의 port 찾기
-    function find_idle_port() {
-        IDLE_PROFILE=$(find_idle_profile)
+# 쉬고 있는 profile의 port 찾기
+function find_idle_port() {
+    IDLE_PROFILE=$(find_idle_profile)
 
-        if [ ${IDLE_PROFILE} == real1 ]
-        then
-          echo "8081"
-        else
-          echo "8082"
-        fi
-    }
+    if [ ${IDLE_PROFILE} == real1 ]
+    then
+      echo "8081"
+    else
+      echo "8082"
+    fi
 }
